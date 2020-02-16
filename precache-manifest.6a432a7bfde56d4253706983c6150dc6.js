9e47636beb0794a32928183a7dd4d3da497d73f9
namdo1012 <namdo1012199@gmail.com>
2020-02-16 21:21:41 +0700

    Updates

---
+++ precache-manifest.6a432a7bfde56d4253706983c6150dc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73a1d12e5eeb885076c3417ba627773",
+    "url": "/mymovie-app/index.html"
+  },
+  {
+    "revision": "94571bb5936653485a68",
+    "url": "/mymovie-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "db9198991db42c1961ff",
+    "url": "/mymovie-app/static/css/main.e1ad3651.chunk.css"
+  },
+  {
+    "revision": "94571bb5936653485a68",
+    "url": "/mymovie-app/static/js/2.7c695b09.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/mymovie-app/static/js/2.7c695b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db9198991db42c1961ff",
+    "url": "/mymovie-app/static/js/main.9b960271.chunk.js"
+  },
+  {
+    "revision": "e6c0dd704388ddb78611",
+    "url": "/mymovie-app/static/js/runtime-main.bf43c197.js"
+  }
+]);
